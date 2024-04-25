ce4206750b04386b70e9ec4524a728536424638e
pjc-dev <64868232+PedroJColon@users.noreply.github.com>
2024-04-25 03:16:39 -0400

    Change keybind to using Meta instead of Leader

--- lua/plugins/fterm.lua
+++ lua/plugins/fterm.lua
@@ -22,7 +22,7 @@ return {
   -- Example keybindings
   vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>'),
   vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>'),
-  vim.keymap.set("n", "<leader>ll", function()
+  vim.keymap.set("n", "<A-l>", function()
     lazygit:toggle()
   end),
 }
